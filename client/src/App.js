6ae060916a3f0e001b65c2d95362b53c117aa778
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-03-20 10:06:53 -0700

    add husky and lint staged

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,29 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { theme } from "./themes/theme.js";
 // import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 import "./App.css";
 
 function App() {
-  const [loggedIn, setLoggedIn] = React.useState(localStorage.getItem("user"));
+	const [loggedIn, setLoggedIn] = React.useState(
+		localStorage.getItem("user")
+	);
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route exact path="/">
-          <Redirect to="/signup" />
-        </Route>
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+	return (
+		<MuiThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Route path="/login" component={Login} />
+				<Route path="/signup" component={Signup} />
+				<Route path="/dashboard" component={Dashboard} />
+				<Route exact path="/">
+					<Redirect to="/signup" />
+				</Route>
+			</BrowserRouter>
+		</MuiThemeProvider>
+	);
 }
 
 export default App;
