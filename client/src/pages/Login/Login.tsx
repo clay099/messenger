d4f21c248f7bd6f2875b6f84856e2f021daadf79
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-03-21 22:20:21 -0700

    further refefactor & add tests

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Snackbar, { SnackbarCloseReason } from "@material-ui/core/Snackbar";
@@ -9,18 +8,39 @@ import { Link, useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import { Formik } from "formik";
-import * as Yup from "yup";
+import { FormikHelpers } from "formik";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "../../styles/useStyles";
 import useLogin from "../../hooks/useLogin";
 import SideBanner from "../../components/SideBanner/SideBanner";
+import LoginForm from "./LoginForm/LoginForm";
 
 export default function Login() {
 	const classes = useStyles();
 	const [open, setOpen] = React.useState(true);
 
 	const history = useHistory();
+
+	const handleSubmit = (
+		{ email, password }: { email: string; password: string },
+		{
+			setStatus,
+			setSubmitting,
+		}: FormikHelpers<{ email: string; password: string }>
+	) => {
+		setStatus();
+		login(email, password).then(
+			() => {
+				// useHistory push to chat
+				console.log(email, password);
+				return;
+			},
+			(error) => {
+				setSubmitting(false);
+				setStatus(error);
+			}
+		);
+	};
 
 	React.useEffect(() => {
 		const user = localStorage.getItem("user");
@@ -82,133 +102,7 @@ export default function Login() {
 								</Typography>
 							</Grid>
 						</Grid>
-						<Formik
-							initialValues={{
-								email: "",
-								password: "",
-							}}
-							validationSchema={Yup.object().shape({
-								email: Yup.string()
-									.required("Email is required")
-									.email("Email is not valid"),
-								password: Yup.string()
-									.required("Password is required")
-									.max(100, "Password is too long")
-									.min(6, "Password too short"),
-							})}
-							onSubmit={(
-								{ email, password },
-								{ setStatus, setSubmitting }
-							) => {
-								setStatus();
-								login(email, password).then(
-									() => {
-										// useHistory push to chat
-										console.log(email, password);
-										return;
-									},
-									(error) => {
-										setSubmitting(false);
-										setStatus(error);
-									}
-								);
-							}}
-						>
-							{({
-								handleSubmit,
-								handleChange,
-								values,
-								touched,
-								errors,
-							}) => (
-								<form
-									onSubmit={handleSubmit}
-									className={classes.form}
-									noValidate
-								>
-									<TextField
-										id="email"
-										label={
-											<p className={classes.label}>
-												E-mail address
-											</p>
-										}
-										fullWidth
-										margin="normal"
-										InputLabelProps={{
-											shrink: true,
-										}}
-										InputProps={{
-											classes: { input: classes.inputs },
-										}}
-										name="email"
-										autoComplete="email"
-										autoFocus
-										helperText={
-											touched.email ? errors.email : ""
-										}
-										error={
-											touched.email &&
-											Boolean(errors.email)
-										}
-										value={values.email}
-										onChange={handleChange}
-									/>
-									<TextField
-										id="password"
-										label={
-											<Typography
-												className={classes.label}
-											>
-												Password
-											</Typography>
-										}
-										fullWidth
-										margin="normal"
-										InputLabelProps={{
-											shrink: true,
-										}}
-										InputProps={{
-											classes: { input: classes.inputs },
-											endAdornment: (
-												<Typography
-													className={classes.forgot}
-												>
-													Forgot?
-												</Typography>
-											),
-										}}
-										type="password"
-										autoComplete="current-password"
-										helperText={
-											touched.password
-												? errors.password
-												: ""
-										}
-										error={
-											touched.password &&
-											Boolean(errors.password)
-										}
-										value={values.password}
-										onChange={handleChange}
-									/>
-
-									<Box textAlign="center">
-										<Button
-											type="submit"
-											size="large"
-											variant="contained"
-											color="primary"
-											className={classes.submit}
-										>
-											Login
-										</Button>
-									</Box>
-
-									<div style={{ height: 95 }} />
-								</form>
-							)}
-						</Formik>
+						<LoginForm handleSubmit={handleSubmit} />
 					</Box>
 					<Box p={1} alignSelf="center" />
 				</Box>
