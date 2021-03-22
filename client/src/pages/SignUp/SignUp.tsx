08387baaa0f083b7f2cc9511eda38baeb2b9888b
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-03-21 22:20:21 -0700

    further refefactor & add tests

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Snackbar, { SnackbarCloseReason } from "@material-ui/core/Snackbar";
@@ -9,12 +8,14 @@ import { Link, useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import { Formik } from "formik";
-import * as Yup from "yup";
+import { FormikHelpers } from "formik";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "../../styles/useStyles";
 import useRegister from "../../hooks/useRegister";
 import SideBanner from "../../components/SideBanner/SideBanner";
+import SignUpForm from "./SignUpForm/SignUpForm";
+
+export interface handleSubmit {}
 
 export default function Register() {
 	const classes = useStyles();
@@ -35,6 +36,31 @@ export default function Register() {
 	};
 
 	const history = useHistory();
+
+	const handleSubmit = (
+		{
+			username,
+			email,
+			password,
+		}: { email: string; password: string; username: string },
+		{
+			setStatus,
+			setSubmitting,
+		}: FormikHelpers<{ email: string; password: string; username: string }>
+	) => {
+		setStatus();
+		register(username, email, password).then(
+			() => {
+				// useHistory push to chat
+				console.log(email, password);
+				return;
+			},
+			(error) => {
+				setSubmitting(false);
+				setStatus(error);
+			}
+		);
+	};
 
 	React.useEffect(() => {
 		const user = localStorage.getItem("user");
@@ -69,7 +95,6 @@ export default function Register() {
 							</Button>
 						</Link>
 					</Box>
-
 					<Box width="100%" maxWidth={450} p={3} alignSelf="center">
 						<Grid container>
 							<Grid item xs>
@@ -82,161 +107,7 @@ export default function Register() {
 								</Typography>
 							</Grid>
 						</Grid>
-						<Formik
-							initialValues={{
-								email: "",
-								password: "",
-								username: "",
-							}}
-							validationSchema={Yup.object().shape({
-								username: Yup.string()
-									.required("Username is required")
-									.max(40, "Username is too long"),
-								email: Yup.string()
-									.required("Email is required")
-									.email("Email is not valid"),
-								password: Yup.string()
-									.required("Password is required")
-									.max(100, "Password is too long")
-									.min(6, "Password too short"),
-							})}
-							onSubmit={(
-								{ username, email, password },
-								{ setStatus, setSubmitting }
-							) => {
-								setStatus();
-								register(username, email, password).then(
-									() => {
-										// useHistory push to chat
-										console.log(email, password);
-										return;
-									},
-									(error) => {
-										setSubmitting(false);
-										setStatus(error);
-									}
-								);
-							}}
-						>
-							{({
-								handleSubmit,
-								handleChange,
-								values,
-								touched,
-								errors,
-							}) => (
-								<form
-									onSubmit={handleSubmit}
-									className={classes.form}
-									noValidate
-								>
-									<TextField
-										id="username"
-										label={
-											<Typography
-												className={classes.label}
-											>
-												Username
-											</Typography>
-										}
-										fullWidth
-										margin="normal"
-										InputLabelProps={{
-											shrink: true,
-										}}
-										InputProps={{
-											classes: { input: classes.inputs },
-										}}
-										name="username"
-										autoComplete="username"
-										autoFocus
-										helperText={
-											touched.username
-												? errors.username
-												: ""
-										}
-										error={
-											touched.username &&
-											Boolean(errors.username)
-										}
-										value={values.username}
-										onChange={handleChange}
-									/>
-									<TextField
-										id="email"
-										label={
-											<Typography
-												className={classes.label}
-											>
-												E-mail address
-											</Typography>
-										}
-										fullWidth
-										margin="normal"
-										InputLabelProps={{
-											shrink: true,
-										}}
-										InputProps={{
-											classes: { input: classes.inputs },
-										}}
-										name="email"
-										autoComplete="email"
-										helperText={
-											touched.email ? errors.email : ""
-										}
-										error={
-											touched.email &&
-											Boolean(errors.email)
-										}
-										value={values.email}
-										onChange={handleChange}
-									/>
-									<TextField
-										id="password"
-										label={
-											<Typography
-												className={classes.label}
-											>
-												Password
-											</Typography>
-										}
-										fullWidth
-										margin="normal"
-										InputLabelProps={{
-											shrink: true,
-										}}
-										InputProps={{
-											classes: { input: classes.inputs },
-										}}
-										type="password"
-										autoComplete="current-password"
-										helperText={
-											touched.password
-												? errors.password
-												: ""
-										}
-										error={
-											touched.password &&
-											Boolean(errors.password)
-										}
-										value={values.password}
-										onChange={handleChange}
-									/>
-
-									<Box textAlign="center">
-										<Button
-											type="submit"
-											size="large"
-											variant="contained"
-											color="primary"
-											className={classes.submit}
-										>
-											Create
-										</Button>
-									</Box>
-								</form>
-							)}
-						</Formik>
+						<SignUpForm handleSubmit={handleSubmit} />
 					</Box>
 					<Box p={1} alignSelf="center" />
 				</Box>
