d73cf72dffd0eb2c871801b27f62931b435105be
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-04-16 17:10:12 -0700

    better error for no jwt

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,6 +7,15 @@ function authenticateJWT(req, res, next) {
 	try {
 		// get token from cookies or body
 		const token = req.cookies?.token || req.headers?.cookie?.token;
+
+		if (!token)
+			return next(
+				createError(
+					400,
+					"Token not found in cookies, please login again to update your cookies"
+				)
+			);
+
 		const payload = jwt.verify(token, config.JWT_SECRET_KEY); // make sure token wasn't messed with
 		// assuming payload is verified you can now grab the user email by "req.user.email" from any route which is authenticated
 		req.user = payload;
