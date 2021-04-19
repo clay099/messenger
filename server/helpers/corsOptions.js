2fba105d7baa0c83d9cc83ff715613f9d55f2eca
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-04-18 19:00:36 -0700

    update to netlify site

--- server/helpers/corsOptions.js
+++ server/helpers/corsOptions.js
@@ -1,5 +1,8 @@
 const corsOptions = {
-	origin: ["http://localhost:3000", "https://messenger-front-end.vercel.app"],
+	origin: [
+		"http://localhost:3000",
+		"https://laughing-brown-e9e4f0.netlify.app",
+	],
 	methods: ["GET", "POST", "PATCH"],
 	credentials: true,
 };
