a1a6bf8e6893d12186832d16c0175ffeabac7eda
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-04-20 10:41:42 -0700

    fix datetime tests

--- client/src/helpers/dateToTime.test.ts
+++ client/src/helpers/dateToTime.test.ts
@@ -4,36 +4,43 @@ describe("dateToTime Test", () => {
 	test("should format seconds", () => {
 		const time = new Date();
 		time.setSeconds(time.getSeconds() - 2);
-		expect(dateToTime(time)).toBe("2 seconds ago");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("2 seconds ago");
 	});
 	test("should format minutes", () => {
 		const time = new Date();
 		time.setMinutes(time.getMinutes() - 2);
-		expect(dateToTime(time)).toBe("2 minutes ago");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("2 minutes ago");
 	});
 	test("should format hours", () => {
 		const time = new Date();
 		time.setHours(time.getHours() - 2);
-		expect(dateToTime(time)).toBe("2 hours ago");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("2 hours ago");
 	});
 	test("should format days", () => {
 		const time = new Date();
 		time.setHours(time.getHours() - 24 * 2);
-		expect(dateToTime(time)).toBe("2 days ago");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("2 days ago");
 	});
 	test("should format one weeks", () => {
 		const time = new Date();
 		time.setHours(time.getHours() - 24 * 7);
-		expect(dateToTime(time)).toBe("last week");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("last week");
 	});
 	test("should format multiple weeks", () => {
 		const time = new Date();
 		time.setHours(time.getHours() - 24 * 7 * 2);
-		expect(dateToTime(time)).toBe("2 weeks ago");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("2 weeks ago");
 	});
 	test("should format years", () => {
 		const time = new Date();
 		time.setHours(time.getHours() - 24 * 7 * 53);
-		expect(dateToTime(time)).toBe("last year");
+		const timeString = time.toString();
+		expect(dateToTime(timeString)).toBe("last year");
 	});
 });
